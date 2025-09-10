eb15680a7c9fe67cdac147dbef3513aec567151c
cagatay <142677193+cagataykoca06@users.noreply.github.com>
2025-09-10 16:44:02 +0300

    Add files via upload

---
+++ haircutcalculators.py
@@ -0,0 +1,51 @@
+hairstyles = ["bouffant", "pixie", "dreadlocks", "crew", "bowl", "bob", "mohawk", "flattop"]
+
+prices = [30, 25, 40, 20, 20, 35, 50, 35]
+
+last_week = [2, 3, 5, 8, 4, 4, 6, 2]
+
+total_price = 0
+for price in prices:
+  total_price += price
+
+print(total_price)
+
+average_price = (total_price / len(prices))
+print("Average Haircut Price: " + str(average_price))
+
+#task5
+new_prices = [] 
+for new_price in prices:
+  x = new_price - 5
+  new_prices.append(x)
+
+print(new_prices)
+
+#task7
+total_revenue = 0
+diversity = len(hairstyles)
+diversity_list = list(range(diversity))
+
+
+
+for i in range (0, diversity):
+  total_revenue += prices[i] * last_week[i]
+
+print("Total Revenue: " + str(total_revenue))
+  
+average_daily_revenue = total_revenue / 7
+print(average_daily_revenue)
+
+#task12
+cuts_under_30 = []
+for i in range (0, diversity):
+  if new_prices[i] < 30:
+    cuts_under_30.append(hairstyles[i])
+
+print(cuts_under_30)
+
+
+
+
+
+
---
+++ total_travel_cost_calculator.py
@@ -0,0 +1,14 @@
+# Write your code below: 
+
+def calculate_expenses(plane_ticket_price, car_rental_rate, hotel_rate, trip_time):
+  car_rental_total = car_rental_rate * trip_time
+  hotel_total = (hotel_rate * trip_time) - 10
+  trip_total = car_rental_total + hotel_total + plane_ticket_price
+  print(trip_total)
+
+
+  
+
+  return trip_total
+
+calculate_expenses(200, 100, 100, 8)
